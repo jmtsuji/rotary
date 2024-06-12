ceb5a1d05fdea74a2841631748e6177992d11718
Lee Bergstrand <leemacboy@gmail.com>
2024-06-11 17:18:13 -0700

    refactor and add documentation for the QC analysis code.

--- rotary/qc.py
+++ rotary/qc.py
@@ -30,34 +30,49 @@ def extract_table_data_from_multiqc_zip(multiqc_zip_path, table_filename='multiq
                 return dataframe
 
 
-def sanatize_fastqc_dataframe(raw_fastqc_dataframe):
-    raw_sample_data_column_name = 'Raw_Sample_Data'
+def sanitize_fastqc_dataframe(raw_fastqc_data: pd.DataFrame, file_types=None, data_columns_to_include=None):
+    """
+    This method performs various cleaning operations on a raw FastQC DataFrame.
+    Various columns are converted from strings to numeric.
+    The sample column is split into three columns for sample name, stage, and direction.
+
+    :param raw_fastqc_data: The raw FastQC DataFrame that needs to be sanitized.
+    :param file_types: The strings used in the file names to designate different QC file types.
+    :param data_columns_to_include: The data columns to include in sanitized DataFrame.
+    :return: The cleaned FastQC DataFram that contains only the desired columns.
+    """
+    if data_columns_to_include is None:
+        data_columns_to_include = ['Sample', 'Stage', 'Direction', 'Total Sequences',
+                                   'Total Bases (Mbp)', 'Sequence Length Min', 'Sequence Length Max',
+                                   'avg_sequence_length', 'median_sequence_length', '%GC']
+    if file_types is None:
+        file_types = ['_adapter_trim', '_reformat', '_quality_trim', '_reformat', '_filter']
 
     # Rename the sample column to a more general name because it will be split into multiple columns.
-    raw_fastqc_dataframe.rename(columns={'Sample': raw_sample_data_column_name}, inplace=True)
+    sample_data_column_name = 'Raw_Sample_Data'
+    raw_fastqc_data.rename(columns={'Sample': sample_data_column_name}, inplace=True)
 
-    # The QC file type designators that are included in the QC file names.
-    file_types = ['_adapter_trim', '_reformat', '_quality_trim', '_reformat', '_filter']
-
-    generate_fastqc_sample_name_stage_and_direction_columns(raw_fastqc_dataframe, raw_sample_data_column_name,
+    generate_fastqc_sample_name_stage_and_direction_columns(raw_fastqc_data, sample_data_column_name,
                                                             file_types)
+    convert_string_fastqc_data_to_numeric(raw_fastqc_data)
 
-    convert_string_fastqc_data_to_numeric(raw_fastqc_dataframe)
-
-    cleaned_fastqc_dataframe = raw_fastqc_dataframe[['Sample', 'Stage', 'Direction', 'Total Sequences',
-                                                     'Total Bases (Mbp)', 'Sequence Length Min', 'Sequence Length Max',
-                                                     'avg_sequence_length', 'median_sequence_length', '%GC']]
-
-    return cleaned_fastqc_dataframe
+    return raw_fastqc_data[data_columns_to_include]
 
 
-def generate_fastqc_sample_name_stage_and_direction_columns(fastqc_dataframe, sample_column_name, file_types):
-    sample_name_no_direction_column_name = 'Sample_Name_No_Direction'
+def generate_fastqc_sample_name_stage_and_direction_columns(fastqc_data: pd.DataFrame, sample_column, file_types):
+    """
+    Splits the input sample column into sample name, type, and direction columns.
+
+    :param fastqc_data: A pandas DataFrame containing the FastQC data.
+    :param sample_column: The name of the column in the DataFrame that contains the raw sample names.
+    :param file_types: A list of file type strings to remove from the sample names.
+    """
+    temp_sample_no_direction_col_name = 'Sample_Name_No_Direction'
 
     # Split the sample_name column so that the direction of the sample is in its own column.
-    processed_sample_columns = fastqc_dataframe[sample_column_name].str.rsplit('_', n=1, expand=True)
-    processed_sample_columns.columns = [sample_name_no_direction_column_name, 'Direction']
-    sample_column_no_direction = processed_sample_columns[sample_name_no_direction_column_name]
+    processed_sample_columns = fastqc_data[sample_column].str.rsplit('_', n=1, expand=True)
+    processed_sample_columns.columns = [temp_sample_no_direction_col_name, 'Direction']
+    sample_column_no_direction = processed_sample_columns[temp_sample_no_direction_col_name]
 
     # Remove QC file type information from the sample names.
     for qc_file_type_designators in file_types:
@@ -67,23 +82,43 @@ def generate_fastqc_sample_name_stage_and_direction_columns(fastqc_dataframe, sa
     # Generate the QC stage of each sample by removing the sample name
     # from the recently generated sample_column_no_direction column.
     sample_types = processed_sample_columns.apply(
-        lambda row: row[sample_name_no_direction_column_name].replace(row['Sample'], ''), axis=1)
+        lambda row: row[temp_sample_no_direction_col_name].replace(row['Sample'], ''), axis=1)
     processed_sample_columns['Stage'] = sample_types.replace('', 'raw')  # Samples with no sample type should be raw.
-    processed_sample_columns.drop(columns=sample_name_no_direction_column_name, inplace=True)
-    fastqc_dataframe[['Direction', 'Sample', 'Stage']] = processed_sample_columns
+    processed_sample_columns.drop(columns=temp_sample_no_direction_col_name, inplace=True)
+    fastqc_data[['Direction', 'Sample', 'Stage']] = processed_sample_columns
 
 
-def convert_string_fastqc_data_to_numeric(fastqc_dataframe):
+def convert_string_fastqc_data_to_numeric(fastqc_data: pd.DataFrame, total_bases_column='Total Bases',
+                                          sequence_length_column='Sequence length'):
+    """
+    Converts total bases and sequence length columns of FastQC data from string format to numeric format.
+    The total bases column is directly converted to numeric,
+    and the sequence length column is split into min and max sequence length columns.
+
+    :param fastqc_data: A pandas DataFrame containing the FastQC data.
+    :param total_bases_column: The name of column in the DataFrame containing total bases data.
+    :param sequence_length_column: The name of column in the DataFrame containing sequence length data.
+    """
     # Create a new total bases column that uses float values instead of strings.
-    fastqc_dataframe['Total Bases (Mbp)'] = fastqc_dataframe['Total Bases'].str.replace(' Mbp', ' ').astype(
+    fastqc_data['Total Bases (Mbp)'] = fastqc_data[total_bases_column].str.replace(' Mbp', ' ').astype(
         np.float64)
 
     # Split the sequence length column into two columns that two float columns.
-    fastqc_dataframe[['Sequence Length Min', 'Sequence Length Max']] = fastqc_dataframe['Sequence length'].str.split(
-        '-', expand=True).astype(np.float64)
+    fastqc_data[['Sequence Length Min', 'Sequence Length Max']] = fastqc_data[
+        sequence_length_column].str.split('-', expand=True).astype(np.float64)
 
 
-def generate_fastqc_comparison_table(single_direction_table, start_qc_file_type, end_qc_file_type):
+def generate_fastqc_before_and_after_comparison_data(single_direction_fastqc_data: pd.DataFrame,
+                                                     start_file_type, end_file_type):
+    """
+    Generates a comparison from the FastQC data of sequencing data stats before and after QC was performed.
+    Calculates the percentage change of various sequencing data stats before and after QC.
+
+    :param single_direction_fastqc_data: A DataFrame containing FastQC data for a single direction.
+    :param start_file_type: A string representing the start QC file type.
+    :param end_file_type: A string representing the end QC file type.
+    :return: DataFrame containing sequencing data stats before and after QC was performed.
+    """
     qc_stage_column_name = 'Stage'
     name_column_name = 'Sample'
     percentage_change_name = 'Change (%)'
@@ -92,9 +127,9 @@ def generate_fastqc_comparison_table(single_direction_table, start_qc_file_type,
 
     # Filter down the comparison table to just the before and after qc file types.
     # Replace these file type names with before and after QC.
-    before_and_after_qc_table = single_direction_table[single_direction_table[qc_stage_column_name].isin(
-        [start_qc_file_type, end_qc_file_type])].replace(
-        {start_qc_file_type: before_qc_name, end_qc_file_type: after_qc_name}).set_index(name_column_name)
+    before_and_after_qc_table = single_direction_fastqc_data[single_direction_fastqc_data[qc_stage_column_name].isin(
+        [start_file_type, end_file_type])].replace(
+        {start_file_type: before_qc_name, end_file_type: after_qc_name}).set_index(name_column_name)
 
     before_qc_samples = before_and_after_qc_table[before_and_after_qc_table[qc_stage_column_name] == before_qc_name]
     after_qc_samples = before_and_after_qc_table[before_and_after_qc_table[qc_stage_column_name] == after_qc_name]
@@ -107,31 +142,57 @@ def generate_fastqc_comparison_table(single_direction_table, start_qc_file_type,
 
     final_data = pd.concat([before_and_after_qc_table, percentage_change]).sort_index().set_index(qc_stage_column_name,
                                                                                                   append=True)
-
     return final_data
 
 
-def single_direction_fastqc_dataframe(cleaned_fastqc_dataframe, direction):
+def generate_single_direction_fastqc_dataframe(cleaned_fastqc_data: pd.DataFrame, direction):
+    """
+    Generate a DataFrame containing only the data for a single FASTQ direction from the cleaned FASTQC data.
+
+    :param cleaned_fastqc_data: The cleaned FASTQC DataFrame, containing information for one or more directions.
+    :param direction: The direction to filter the data to.
+    :return: A DataFrame containing only the data for the specified direction.
+    """
     direction_column_name = 'Direction'
     # Filter down the comparison table to a certain FASTQ file direction.
-    single_direction_table = cleaned_fastqc_dataframe[cleaned_fastqc_dataframe[direction_column_name] == direction]
+    single_direction_table = cleaned_fastqc_data[cleaned_fastqc_data[direction_column_name] == direction]
     single_direction_table = single_direction_table.drop(columns=direction_column_name)
     return single_direction_table
 
 
-def generate_fastqc_summary(input_multiqc_files, summary_output_dir, out_file_base_name, read_type):
-    fastqc_data = pd.concat([extract_table_data_from_multiqc_zip(multiqc_path) for multiqc_path in input_multiqc_files])
-    sanatized_fastqc_data = sanatize_fastqc_dataframe(fastqc_data)
+def write_fastqc_summary_tsvs(sample_multiqc_paths, summary_output_dir, out_file_base, read_type):
+    """
+    Writes summary TSV files for FastQC data.
+    Writes a single TSV file for long input files.
+    Writes two TSV files for short input files.
+    The two TSV files contain left or right reads, respectively.
+
+    :param sample_multiqc_paths: List of paths to multiqc files for each sample.
+    :param summary_output_dir: Directory where the summary TSV files will be written.
+    :param out_file_base: Base name for the output TSV files.
+    :param read_type: Type of read ('short' or 'long').
+    """
+    raw_fastqc_data = pd.concat([extract_table_data_from_multiqc_zip(path) for path in sample_multiqc_paths])
+    sanitized_fastqc_data = sanitize_fastqc_dataframe(raw_fastqc_data)
 
     if read_type == 'short':
-        write_fastqc_summary_tsv(sanatized_fastqc_data, 'R1', summary_output_dir, out_file_base_name)
-        write_fastqc_summary_tsv(sanatized_fastqc_data, 'R2', summary_output_dir, out_file_base_name)
+        write_before_and_after_comparison_tsv(sanitized_fastqc_data, 'R1', summary_output_dir, out_file_base)
+        write_before_and_after_comparison_tsv(sanitized_fastqc_data, 'R2', summary_output_dir, out_file_base)
     else:
-        write_fastqc_summary_tsv(sanatized_fastqc_data, 'long', summary_output_dir, out_file_base_name)
+        write_before_and_after_comparison_tsv(sanitized_fastqc_data, 'long', summary_output_dir, out_file_base)
 
 
-def write_fastqc_summary_tsv(cleaned_fastqc_data, direction, summary_output_dir, out_file_name):
-    output_path = os.path.join(summary_output_dir, f'{out_file_name}_{direction}.tsv')
-    fastqc_dataframe = single_direction_fastqc_dataframe(cleaned_fastqc_data, direction)
-    comparison_data = generate_fastqc_comparison_table(fastqc_dataframe, 'raw', '_filter')
+def write_before_and_after_comparison_tsv(cleaned_fastqc_data: pd.DataFrame, direction, summary_output_dir,
+                                          out_file_base):
+    """
+    This method writes a before and after comparison TSV file for the specified direction using the cleaned FastQC data.
+
+    :param cleaned_fastqc_data: The cleaned FASTQC DataFrame.
+    :param direction: The direction of the data to filter to (e.g., 'R1', 'R2' or 'long').
+    :param summary_output_dir: The directory where the summary output file will be saved.
+    :param out_file_base: The base file name for the summary output file.
+    """
+    output_path = os.path.join(summary_output_dir, f'{out_file_base}_{direction}.tsv')
+    fastqc_dataframe = generate_single_direction_fastqc_dataframe(cleaned_fastqc_data, direction)
+    comparison_data = generate_fastqc_before_and_after_comparison_data(fastqc_dataframe, 'raw', '_filter')
     comparison_data.to_csv(output_path, sep='\t', index=True)
